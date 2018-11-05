2ddde8e329c4e1bf4561d80eb5a29e827c28ba20
Sam Gardner <samcgardner@gmail.com>
2018-11-05 01:07:15 +0000

    Add main that prints fizz buzzes of numbers

--- src/main/java/com/samcgardner/pinkflamingo/PinkFlamingo.java
+++ src/main/java/com/samcgardner/pinkflamingo/PinkFlamingo.java
@@ -1,9 +1,14 @@
 package com.samcgardner.pinkflamingo;
 
+import java.util.stream.IntStream;
+
 public class PinkFlamingo {
 
     public static void main(String[] args) {
-
+        IntStream.range(0, 101)
+                .boxed()
+                .map(PinkFlamingo::fizzBuzzWithPinkFlamingo)
+                .forEach(System.out::println);
     }
 
     public static String fizzBuzzWithPinkFlamingo(int n) {
