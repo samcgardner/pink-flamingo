d7cb0f0cf02e518cca136a1b60a55b018d9403c4
Sam Gardner <samcgardner@gmail.com>
2018-11-05 02:02:30 +0000

    Add trivial passing test for dropwizard project

--- src/main/java/com/samcgardner/pinkflamingo/resource/PinkFlamingoResource.java
+++ src/main/java/com/samcgardner/pinkflamingo/resource/PinkFlamingoResource.java
@@ -2,6 +2,7 @@ package com.samcgardner.pinkflamingo.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ public class PinkFlamingoResource {
 
     @GET
     @Path("pink-flamingo")
+    @Produces("application/json")
     public List<String> pinkFlamingo() {
         return new LinkedList<>();
     }
---
+++ src/test/java/com/samcgardner/pinkflamingo/PinkFlamingoApplicationTest.java
@@ -0,0 +1,36 @@
+package com.samcgardner.pinkflamingo;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+public class PinkFlamingoApplicationTest {
+
+    @ClassRule
+    public static final DropwizardAppRule<Configuration> RULE =
+            new DropwizardAppRule<>(PinkFlamingoApplication.class);
+
+    @Test
+    public void returnsListOfPinkFlamingos() {
+        Client client = JerseyClientBuilder.createClient();
+
+        Response response = client.target(
+                String.format("http://localhost:%d/pink-flamingo", RULE.getLocalPort()))
+                .queryParam("from", 0)
+                .queryParam("to", 11)
+                .request()
+                .get();
+
+        assertEquals(200, response.getStatus());
+        assertEquals("[]", response.readEntity(String.class));
+    }
+
+}
+
