740e9e48102b3beac0b3ab28c26fdf4cc3415fef
Sam Gardner <samcgardner@gmail.com>
2018-11-05 01:30:07 +0000

    Very broken implementation of pink flamingo

--- src/main/java/com/samcgardner/pinkflamingo/PinkFlamingo.java
+++ src/main/java/com/samcgardner/pinkflamingo/PinkFlamingo.java
@@ -14,6 +14,9 @@ public class PinkFlamingo {
     public static String fizzBuzzWithPinkFlamingo(int n) {
         boolean isMultipleOfThree = isMultipleOf(n, 3);
         boolean isMultipleOfFive = isMultipleOf(n, 5);
+        boolean isFib = isFib(n);
+        if (isFib && isMultipleOfFive && isMultipleOfThree) return "Pink Flamingo";
+        if (isFib) return "Flamingo";
         if (isMultipleOfThree && isMultipleOfFive) return "FizzBuzz";
         if (isMultipleOfThree) return "Fizz";
         if (isMultipleOfFive) return "Buzz";
@@ -22,6 +25,16 @@ public class PinkFlamingo {
 
     private static boolean isMultipleOf(int input, int divisor) {
         return input % divisor == 0;
+    }
+
+    private static boolean isFib(int n) {
+        return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4);
+    }
+
+    // This isn't correct for very large n, so if we can't find any pink flamingos above a certain bound, that's why
+    private static boolean isPerfectSquare(int n) {
+        double sqrt = Math.sqrt(n);
+        return sqrt * sqrt == n;
     }
 
 }
